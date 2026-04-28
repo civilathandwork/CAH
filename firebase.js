4492cba34fb89257ec512aa8b9872d9370e5ffff
civilathandwork <civilathand.work@gmail.com>
2026-04-28 11:27:53 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR KEY",
-  authDomain: "YOUR DOMAIN",
-  projectId: "YOUR ID",
+  apiKey: "AIzaSyB5Tnxu44OmT72EJPAKl6QtdgO7-OtP3RI",
+  authDomain: "civil-at-hand-3cc14.firebaseapp.com",
+  projectId: "civil-at-hand-3cc14",
+  storageBucket: "civil-at-hand-3cc14.firebasestorage.app",
+  messagingSenderId: "399251861477",
+  appId: "1:399251861477:web:a2711a99f8a9d9c398090c"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const db = getFirestore(app); // ⭐ NEW
